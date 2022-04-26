162bc1cfa0fc6d991f840b806ff6f08893c3a328
woozxn <squib0612@gmail.com>
2022-04-26 17:29:31 +0900

    22.04.25

--- src/Programmers/Level2/Tuple.java
+++ src/Programmers/Level2/Tuple.java
@@ -1,5 +1,23 @@
 package Programmers.Level2;
 //튜플
+import java.util.*;
+class Tuple {
+    public int[] solution(String s) {
+        Set<String> set = new HashSet<>();
+        String[] arr = s.replaceAll("[{]", " ").replaceAll("[}]", " ").trim().split(" , ");
+        Arrays.sort(arr, (a, b)->{return a.length() - b.length();});
+        int[] answer = new int[arr.length];
+        int idx = 0;
+        for(String s1 : arr) {
+            for(String s2 : s1.split(",")) {
+                if(set.add(s2)) answer[idx++] = Integer.parseInt(s2);
+            }
+        }
+        return answer;
+    }
+}
+
+
 //import java.util.*;
 //class Tuple{
 //    public int[] solution(String s) {
