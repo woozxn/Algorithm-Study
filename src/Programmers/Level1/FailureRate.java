d4b50531cbf495635bddd27a36678d351ea5db4a
woozxn <squib0612@gmail.com>
2022-05-06 23:47:32 +0900

    22.05.06

---
+++ src/Programmers/Level1/FailureRate.java
@@ -0,0 +1,41 @@
+package Programmers.Level1;
+//실패율
+public class FailureRate {
+    public int[] failureRate(int N, int[] stages) {
+        int[] answer = new int[N];
+        double[] tempArr = new double[N];
+        int arrLength = stages.length;
+        int idx = arrLength;
+        double tempD = 0;
+        int tempI = 0;
+
+        for (int i = 0; i < arrLength; i++) {
+            int stage = stages[i];
+            if (stage != N + 1)
+                answer[stage - 1] += 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int personNum = answer[i];
+            tempArr[i] = (double) personNum / idx;
+            idx -= personNum;
+            answer[i] = i + 1;
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < N - i; j++) {
+                if (tempArr[j - 1] < tempArr[j]) {
+                    tempD = tempArr[j - 1];
+                    tempArr[j - 1] = tempArr[j];
+                    tempArr[j] = tempD;
+
+                    tempI = answer[j - 1];
+                    answer[j - 1] = answer[j];
+                    answer[j] = tempI;
+                }
+            }
+        }
+        return answer;
+    }
+}
+
