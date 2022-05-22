e443cc01d2b06864eddd14ccbff4bb40fce42d74
woozxn <squib0612@gmail.com>
2022-05-22 23:13:17 +0900

    22.05.22

---
+++ src/Programmers/Level1/ReportResult.java
@@ -0,0 +1,33 @@
+package Programmers.Level1;
+//신고결과받기
+import java.util.*;
+class ReportResult {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        int[] answer = new int[id_list.length];
+        Map<String, HashSet<String>> map = new HashMap<>();
+        Map<String, Integer> idxMap = new HashMap<>();
+
+        for (int i = 0; i < id_list.length; i++) {
+            String name = id_list[i];
+            map.put(name, new HashSet<>());
+            idxMap.put(name, i);
+        }
+
+        for (String s : report) {
+            String[] str = s.split(" ");
+            String from = str[0];
+            String to = str[1];
+            map.get(to).add(from);
+        }
+
+        for (int i = 0; i < id_list.length; i++) {
+            HashSet<String> send = map.get(id_list[i]);
+            if (send.size() >= k) {
+                for (String name : send) {
+                    answer[idxMap.get(name)]++;
+                }
+            }
+        }
+        return answer;
+    }
+}
