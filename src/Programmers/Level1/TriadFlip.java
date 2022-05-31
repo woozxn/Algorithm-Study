d5c9b251e5466bbf9434fe18eecb1886c69cda49
woozxn <squib0612@gmail.com>
2022-05-31 23:18:13 +0900

    22.05.31
    3진법 뒤집기 추가

--- src/Programmers/Level1/TriadFlip.java
+++ src/Programmers/Level1/TriadFlip.java
@@ -1,5 +1,5 @@
 package Programmers.Level1;
-
+//3진법 뒤집기
 class TriadFlip {
     public int solution(int n) {
         String a = "";
@@ -14,3 +14,19 @@ class TriadFlip {
         return Integer.parseInt(a,3);
     }
 }
+//class Solution {
+//    public int solution(int n) {
+//        int answer = 0;
+//        String third = Integer.toString(n, 3);
+//        StringBuffer sb = new StringBuffer(third);
+//        String reversed = sb.reverse().toString();
+//
+//        int exp = 0;
+//        for (int i = reversed.length() - 1; i >= 0; i--) {
+//            answer += Integer.parseInt(String.valueOf(reversed.charAt(i))) * Math.pow(3, exp);
+//            exp++;
+//        }
+//
+//        return answer;
+//    }
+//}
