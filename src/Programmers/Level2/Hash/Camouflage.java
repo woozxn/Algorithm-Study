5b3bd8949e2360659d5902d421bce0dd84255359
woozxn <squib0612@gmail.com>
2022-06-07 22:50:01 +0900

    22.06.07
    위장

---
+++ src/Programmers/Level2/Hash/Camouflage.java
@@ -0,0 +1,14 @@
+package Programmers.Level2.Hash;
+//위장
+import java.util.*;
+import static java.util.stream.Collectors.*;
+
+class Camouflage {
+    public int solution(String[][] clothes) {
+        return Arrays.stream(clothes)
+                .collect(groupingBy(p -> p[1], mapping(p -> p[0], counting())))
+                .values()
+                .stream()
+                .collect(reducing(1L, (x, y) -> x * (y + 1))).intValue() - 1;
+    }
+}
