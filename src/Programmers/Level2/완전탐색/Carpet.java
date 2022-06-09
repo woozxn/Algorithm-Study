aaefceb24a527d544fafdc18b70926a00b4d680f
woozxn <squib0612@gmail.com>
2022-06-09 22:53:04 +0900

    22.06.09

--- src/Programmers/Level2/완전탐색/Carpet.java
+++ src/Programmers/Level2/완전탐색/Carpet.java
@@ -9,3 +9,21 @@ class Carpet {
         return answer;
     }
 }
+//class Carpet {
+//    public int[] solution(int brown, int red) {
+//        int[] answer = {};
+//        answer = new int[2];
+//        int iAllNum = brown + red;
+//        int iHeight = 0;
+//
+//        for (int iBrownWidth = 1; iBrownWidth < brown; iBrownWidth++) {
+//            iHeight = iAllNum/iBrownWidth;
+//
+//            if((iBrownWidth-2)*(iHeight-2) == red) {
+//                answer[0] = iBrownWidth;
+//                answer[1] = iHeight;
+//            }
+//        }
+//        return answer;
+//    }
+//}
