b2342ebcf814a140239a3cd655474dabfd1601a8
woozxn <squib0612@gmail.com>
2023-01-06 23:32:53 +0900

    22.01.06

---
+++ src/Baekjoon/Greedy/StarBox.java
@@ -0,0 +1,32 @@
+//1758 알바생 강호
+package Baekjoon.Greedy;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class StarBox {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        Integer[] money = new Integer[n];
+        long answer = 0;
+
+        for(int i=0; i<n; i++) {
+            money[i] = sc.nextInt();
+        }
+        Arrays.sort(money, Comparator.reverseOrder());
+
+        for(int k=0; k<n; k++) {
+            int tip = money[k] - k;
+            if(tip < 0) {
+                break;
+            }
+            answer += tip;
+        }
+
+        System.out.println(answer);
+    }
+
+}
