941ac96f5c1a48d942dc51a91f8ddca63c3f7e87
woozxn <squib0612@gmail.com>
2023-01-13 23:09:29 +0900

    23.01.13

--- src/Baekjoon/Greedy/StarBox.java
+++ src/Baekjoon/Greedy/StarBox.java
@@ -7,16 +7,16 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 public class StarBox {
-    public static void main(String[] args) throws NumberFormatException, IOException {
+    public static void main(String[] args) throws NumberFormatException {    //NumberFormatException?
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         Integer[] money = new Integer[n];
-        long answer = 0;
+        long answer = 0;                                // 답 범위 때문에 long 형으로 해줘야 하는건가?
 
         for(int i=0; i<n; i++) {
             money[i] = sc.nextInt();
         }
-        Arrays.sort(money, Comparator.reverseOrder());
+        Arrays.sort(money, Comparator.reverseOrder()); // Integer 배열이어야 내림차순 정렬 가능?? -> Yes
 
         for(int k=0; k<n; k++) {
             int tip = money[k] - k;
