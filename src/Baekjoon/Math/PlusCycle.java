d8d0117e91eb51dde537ae53321c5e9d1402ea7d
woozxn <squib0612@gmail.com>
2023-02-12 23:45:08 +0900

    23.02.12

--- src/Baekjoon/Math/PlusCycle.java
+++ src/Baekjoon/Math/PlusCycle.java
@@ -1,4 +1,25 @@
 package Baekjoon.Math;
 
+import java.util.Scanner;
+
 public class PlusCycle {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int answer = N;
+
+        int count = 0;
+        while(true) {
+            if(N<10) {
+                N = 10*N + N;
+            } else {
+                N = ((N%10) * 10) + ((N%10 + N/10)%10);
+            }
+            count++;
+            if(answer == N) break;
+        }
+
+        System.out.println(count);
+    }
 }
